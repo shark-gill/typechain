883a4c791365c1d18f81dbca3599674cc19d2078
shark-gill <lhshrk4@gmail.com>
2020-02-09 21:20:21 +0900

    finish typeChain

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi("1", 2, "3"));
-//# sourceMappingURL=index.js.map
